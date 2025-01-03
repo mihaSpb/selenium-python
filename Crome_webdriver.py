cc9a65b6d8fc0d142a9b10f9ddb141b6cfaa2ad8
Mikhail <mihaspb2007@gmail.com>
2025-01-03 12:30:40 +0300

    Add Crome webdriver

--- Crome_webdriver.py
+++ Crome_webdriver.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
