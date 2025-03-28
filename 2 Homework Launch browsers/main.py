92d9498ce0e23eafa0d68f7c8e11cad88e2cc0cb
Mikhail <mihaspb2007@gmail.com>
2025-03-28 18:19:12 +0300

    Update 2 Homework

--- 2 Homework Launch browsers/main.py
+++ 2 Homework Launch browsers/main.py
@@ -1,80 +1,76 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium.common.exceptions import WebDriverException
 
 
-class LaunchBrowser:
+class BaseBrowser:
     def __init__(self, url: str):
         self.url = url
-        self.drivers = {}
-        self.driver_initializers = {
-            "chrome": self.init_chrome,
-            "firefox": self.init_firefox,
-            "edge": self.init_edge
-        }
+        self.driver = None
+
+    def launch(self):
+        if self.driver:
+            self.driver.get(self.url)
+            self.driver.set_window_size(1920, 1080)
+        else:
+            raise RuntimeError("Драйвер не инициализирован")
+
+    def close(self):
+        if self.driver:
+            self.driver.close()
 
 
-    def init_chrome(self):
-        # Инициализирует драйвер для Google Chrome.
+class ChromeBrowser(BaseBrowser):
+    def __init__(self, url: str):
+        super().__init__(url)
+        self.driver = ChromeBrowser.init_driver()
+
+    @staticmethod
+    def init_driver():
         options = webdriver.ChromeOptions()
-        return webdriver.Chrome(service = ChromeService(ChromeDriverManager().install()), options = options)
+        options.add_experimental_option('detach', False)
+        return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 
-    def init_firefox(self):
-        # Инициализирует драйвер для Mozilla Firefox.
+class EdgeBrowser(BaseBrowser):
+    def __init__(self, url: str):
+        super().__init__(url)
+        self.driver = EdgeBrowser.init_driver()
+
+    @staticmethod
+    def init_driver():
+        options = webdriver.EdgeOptions()
+        return webdriver.Edge(service = EdgeService(EdgeChromiumDriverManager().install()), options=options)
+
+
+class FirefoxBrowser(BaseBrowser):
+    def __init__(self, url):
+        super().__init__(url)
+        self.driver = FirefoxBrowser.init_driver()
+
+    @staticmethod
+    def init_driver():
         options = webdriver.FirefoxOptions()
         return webdriver.Firefox(service = FirefoxService(GeckoDriverManager().install()), options = options)
 
 
-    def init_edge(self):
-        # Инициализирует драйвер для Microsoft Edge.
-        options = webdriver.EdgeOptions()
-        return webdriver.Edge(service = EdgeService(EdgeChromiumDriverManager().install()), options = options)
-
-
-    def init_driver(self, browser: str):
-        # Выбор и инициализация драйвера для указанного браузера через словарь инициализаторов.
-        browser = browser.lower()
-        if browser not in self.driver_initializers:
-            raise ValueError(f"Неподдерживаемый браузер: {browser}")
-        return self.driver_initializers[browser]()
-
-
-    def launch_browser_instance(self, driver):
-        # Запускает браузер с заданным URL с устанавленным размером окна.
-        driver.get(self.url)
-        driver.set_window_size(1920, 1080)
-
-
-    def launch_all_browsers(self):
-        # Запускает указанный URL во всех поддерживаемых браузерах с обработкой исключений.
-        browsers = ["chrome", "firefox", "edge"]
-        for browser in browsers:
-            try:
-                print(f"Запуск {browser.capitalize()}...")
-                driver = self.init_driver(browser)
-                self.launch_browser_instance(driver)
-                self.drivers[browser] = driver
-            except WebDriverException as e:
-                print(f"Ошибка при запуске {browser.capitalize()}: {e}")
-            except Exception as e:
-                print(f"Непредвиденная ошибка при запуске {browser.capitalize()}: {e}")
-
-
 if __name__ == "__main__":
     default_url = "https://www.saucedemo.com/"
-    user_input = input(f"Введите URL для открытия (по умолчанию: {default_url}): ").strip()
 
-    if user_input:
-        launcher = LaunchBrowser(user_input)
-        launcher.launch_all_browsers()
-    else:
-        launcher = LaunchBrowser(default_url)
-        launcher.launch_all_browsers()
+    chrome_browser = ChromeBrowser(default_url)
+    edge_browser = EdgeBrowser(default_url)
+    firefox_browser = FirefoxBrowser(default_url)
 
-    input("Нажмите Enter для завершения...")
+    chrome_browser.launch()
+    edge_browser.launch()
+    firefox_browser.launch()
+
+    input("Нажмите Enter для завершения работы программы...")
+
+    chrome_browser.close()
+    edge_browser.close()
+    firefox_browser.close()
