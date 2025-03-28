11ac4be979c4d57d2223cc11a38c0cd7fe72f622
Mikhail <mihaspb2007@gmail.com>
2025-03-28 20:26:39 +0300

    Update 2 Homework

--- 2 Homework Launch browsers/main.py
+++ 2 Homework Launch browsers/main.py
@@ -3,8 +3,8 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class BaseBrowser:
@@ -17,17 +17,17 @@ class BaseBrowser:
             self.driver.get(self.url)
             self.driver.set_window_size(1920, 1080)
         else:
-            raise RuntimeError("Драйвер не инициализирован")
+            raise RuntimeError("Драйвер не инициализирован!")
 
     def close(self):
         if self.driver:
-            self.driver.close()
+            self.driver.quit()
 
 
 class ChromeBrowser(BaseBrowser):
     def __init__(self, url: str):
         super().__init__(url)
-        self.driver = ChromeBrowser.init_driver()
+        self.driver = self.init_driver()
 
     @staticmethod
     def init_driver():
@@ -39,38 +39,46 @@ class ChromeBrowser(BaseBrowser):
 class EdgeBrowser(BaseBrowser):
     def __init__(self, url: str):
         super().__init__(url)
-        self.driver = EdgeBrowser.init_driver()
+        self.driver = self.init_driver()
 
     @staticmethod
     def init_driver():
         options = webdriver.EdgeOptions()
-        return webdriver.Edge(service = EdgeService(EdgeChromiumDriverManager().install()), options=options)
+        return webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
 
 
 class FirefoxBrowser(BaseBrowser):
-    def __init__(self, url):
+    def __init__(self, url: str):
         super().__init__(url)
-        self.driver = FirefoxBrowser.init_driver()
+        self.driver = self.init_driver()
 
     @staticmethod
     def init_driver():
         options = webdriver.FirefoxOptions()
-        return webdriver.Firefox(service = FirefoxService(GeckoDriverManager().install()), options = options)
+        return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+
+
+def wait_for_enter():
+    input("Нажмите Enter, чтобы закрыть браузер и продолжить...")
 
 
 if __name__ == "__main__":
     default_url = "https://www.saucedemo.com/"
 
+    # Запуск Chrome
     chrome_browser = ChromeBrowser(default_url)
-    edge_browser = EdgeBrowser(default_url)
-    firefox_browser = FirefoxBrowser(default_url)
-
     chrome_browser.launch()
-    edge_browser.launch()
-    firefox_browser.launch()
-
-    input("Нажмите Enter для завершения работы программы...")
-
+    wait_for_enter()
     chrome_browser.close()
+
+    # Запуск Edge
+    edge_browser = EdgeBrowser(default_url)
+    edge_browser.launch()
+    wait_for_enter()
     edge_browser.close()
+
+    # Запуск Firefox
+    firefox_browser = FirefoxBrowser(default_url)
+    firefox_browser.launch()
+    wait_for_enter()
     firefox_browser.close()
