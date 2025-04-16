89565669396a8156310e38d956955f826eed85ef
Mikhail <mihaspb2007@gmail.com>
2025-04-16 19:51:56 +0300

    Update 17 Homework

--- 17 Homework Check box/main.py
+++ 17 Homework Check box/main.py
@@ -5,14 +5,13 @@ from test_suite import TestSuite
 
 if __name__ == "__main__":
     default_url = "https://demoqa.com/checkbox"
-    element_locator = "//*[@id='tree-node']/ol/li/span/label/span[1]"
 
     chrome_browser = ChromeBrowser(default_url)
     chrome_browser.launch()
     tests = TestSuite(chrome_browser.driver)
 
     # Тесты
-    tests.universal_click(element_locator, "Select check box")
+    tests.universal_click("//*[@id='tree-node']/ol/li/span/label/span[1]", "Select check box")
     tests.is_element_selected("//*[@id='tree-node-home']")
     time.sleep(2)
 
