98ea476167cb9386eccd2403ba8b551c24648d69
Mikhail <mihaspb2007@gmail.com>
2025-04-28 15:51:20 +0300

    Update class all_test_suite.py

--- clasases/all_test_suite.py
+++ clasases/all_test_suite.py
@@ -191,3 +191,23 @@ class TestSuite:
 
         assert selected.is_selected(), "Element is not selected"
         print("Element is selected")
+
+    def move_slider(self, locator: str, offset_str: str):
+        actions = ActionChains(self.driver)
+        slider = self.driver.find_element(By.XPATH, locator)
+        offset_px = int(offset_str.rstrip('px'))
+
+        actions.click_and_hold(slider).move_by_offset(offset_px,0).release().perform()
+        print(f'Слайдер перемещен на {offset_px}px')
+
+    def check_move_slider(self, slider_locator: str, value_locator: str):
+        slider = self.driver.find_element(By.XPATH, slider_locator)
+        actual = int(slider.get_attribute('value')) # Фактическое значение слайдера
+
+        # Значение поля Value
+        displayed_value = int(self.driver.find_element(By.XPATH, value_locator).text)
+
+        assert displayed_value == actual, (
+            f"В поле Value {displayed_value}, а значение слайдера – {actual}"
+        )
+        print(f'Проверка пройдена: {displayed_value} == {actual}')
