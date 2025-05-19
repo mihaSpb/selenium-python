4184ce74fa0e12672b570221a1fac201e70b00ae
Mikhail <mihaspb2007@gmail.com>
2025-05-19 13:52:37 +0400

    Add 27 Homework

--- 27 Homework Alerts and push notifications/main.py
+++ 27 Homework Alerts and push notifications/main.py
@@ -10,17 +10,6 @@ if __name__ == "__main__":
     chrome_browser.launch()
     tests = TestSuite(chrome_browser.driver)
 
-    push_confirm_button = "//*[@id='content']/div/ul/li[2]/button"
-    push_text_OK = "You clicked: Ok"
-    push_text_cancel = "You clicked: Cancel"
-
-    push_promt_button = "//*[@id='content']/div/ul/li[3]/button"
-    incert_promt = "You entered: "
-    not_incert_promt = "You entered: "
-    promt = "Hi!"
-
-    check_result_text = "//*[@id='result']"
-
     # Alert
     tests.click("//*[@id='content']/div/ul/li[1]/button", "open alert")
     time.sleep(1)
