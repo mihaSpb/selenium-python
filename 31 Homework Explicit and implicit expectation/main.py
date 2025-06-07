dd72c149a99e72158d51af01bebfa9b541b41008
Mikhail <mihaspb2007@gmail.com>
2025-06-07 12:51:25 +0400

    Update 30 Homework. Add explicit wait

---
+++ 31 Homework Explicit and implicit expectation/main.py
@@ -0,0 +1,19 @@
+import time
+from chrome_browser import ChromeBrowser
+from test_suite import TestSuite
+
+
+if __name__ == "__main__":
+    default_url = "https://demoqa.com/dynamic-properties"
+    chrome_browser = ChromeBrowser(default_url)
+    chrome_browser.launch()
+    tests = TestSuite(chrome_browser.driver)
+
+    visible_button = "//*[@id='visibleAfter']"
+
+    # Tests
+    #tests.click_to_invisible_button(visible_button, 'visible button')
+    tests.wait_invisible_button(visible_button, 20, 'visible button')
+    time.sleep(2)
+
+    chrome_browser.close()
---
+++ 31 Homework Explicit and implicit expectation/test_suite.py
@@ -0,0 +1,30 @@
+import time
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class TestSuite:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def click_to_invisible_button(self, locator: str, message: str = ""):
+        try:
+            element = self.driver.find_element(By.XPATH, locator)
+            element.click()
+            print(f"Clicked {message}")
+        except NoSuchElementException:
+            print(f"No such element {locator}")
+            #time.sleep(5)
+            self.driver.refresh()
+            time.sleep(5)
+            element = self.driver.find_element(By.XPATH, locator)
+            element.click()
+            print(f"Clicked {message} after refresh page")
+
+    def wait_invisible_button(self, locator: str, timeout: int, message: str = ""):
+        visible_button = WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((By.XPATH, locator)))
+        visible_button.click()
+        print(f"Clicked {message}")
